a7d018bb854cb2ef1ff7d62bc5ced43e4f2af08d
Aleksey <alekseymn007@gmail.com>
2020-04-03 19:09:44 +0300

    Add project files.

---
+++ TestSonarCloud/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace TestSonarCloud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainLabel = new System.Windows.Forms.Label();
+            this.MainButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MainLabel
+            // 
+            this.MainLabel.AutoSize = true;
+            this.MainLabel.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainLabel.Location = new System.Drawing.Point(12, 9);
+            this.MainLabel.Name = "MainLabel";
+            this.MainLabel.Size = new System.Drawing.Size(81, 33);
+            this.MainLabel.TabIndex = 0;
+            this.MainLabel.Text = "label1";
+            // 
+            // MainButton
+            // 
+            this.MainButton.Font = new System.Drawing.Font("Calibri", 20.25F);
+            this.MainButton.Location = new System.Drawing.Point(12, 396);
+            this.MainButton.Name = "MainButton";
+            this.MainButton.Size = new System.Drawing.Size(116, 42);
+            this.MainButton.TabIndex = 1;
+            this.MainButton.Text = "button1";
+            this.MainButton.UseVisualStyleBackColor = true;
+            this.MainButton.Click += new System.EventHandler(this.MainButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MainButton);
+            this.Controls.Add(this.MainLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MainLabel;
+        private System.Windows.Forms.Button MainButton;
+    }
+}
+
